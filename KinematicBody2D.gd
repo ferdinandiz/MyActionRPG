f696d52eefa924977fb6beb0b10e3853cd57d706
ferdinandiz <80607754+ferdinandiz@users.noreply.github.com>
2022-10-04 20:48:32 -0300

    Sprite da Bullet
    Subindo a sprite

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -6,27 +6,26 @@ func _physics_process(delta):
 	velocidade.y = Input.get_action_strength("baixo") - Input.get_action_strength("cima")
 	move_and_slide(velocidade*100)
 	trocaSprite(velocidade)
-
 	pass
 
 func trocaSprite(vel):
 	if(vel.x == 1):
-		$Sprite.rotation_degrees = 90
+		$".".rotation_degrees = 90
 	if(vel.x == -1):
-		$Sprite.rotation_degrees = 270
+		$".".rotation_degrees = 270
 	if(vel.y == 1):
-		$Sprite.rotation_degrees = 180
+		$".".rotation_degrees = 180
 	if(vel.y == -1):
-		$Sprite.rotation_degrees = 0
+		$".".rotation_degrees = 0
 		
 	if(vel.y == -1 and vel.x == -1):
-		$Sprite.rotation_degrees = 270+45
+		$".".rotation_degrees = 270+45
 	if(vel.y == -1 and vel.x == 1):
-		$Sprite.rotation_degrees = 45
+		$".".rotation_degrees = 45
 	if(vel.y == 1 and vel.x == 1):
-		$Sprite.rotation_degrees = 180-45
+		$".".rotation_degrees = 180-45
 	if(vel.y == 1 and vel.x == -1):
-		$Sprite.rotation_degrees = 180+45
+		$".".rotation_degrees = 180+45
 	pass
 
 
